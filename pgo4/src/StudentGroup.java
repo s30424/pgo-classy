11ea55e83f27c516bb85d33e1ac6b0ba8991e618
s30424 <162446187+s30424@users.noreply.github.com>
2024-04-11 13:47:03 +0200

    Update StudentGroup.java

--- pgo4/src/StudentGroup.java
+++ pgo4/src/StudentGroup.java
@@ -1,41 +1,17 @@
+import java.util.ArrayList;
 public class StudentGroup {
-    public String name;
-    public Student[] students;
-
-    public StudentGroup(String name, Student[] students) {
-        this.name = name;
-        this.students = students;
-    }
-
-    public boolean isInGroup(Student student) {
-        for (Student i : students) {
-            if (i == student) {
-                return true;
-            }
+    public String grupa;
+    ArrayList<Student> lista = new ArrayList<Student>();
+    public void DodajStudenta(Student student){
+        if(lista.contains(student)){
+            throw new IllegalStateException("Taki student juz jest");
         }
-        return false;
+        if (lista.size() <= 15){
+            lista.add(student);
+        }
+        else {
+            throw new IllegalStateException("Za duzo studentow");
+        }
     }
 
-    public Student[] addStudent(Student student) {
-        Student[] studentsNew = new Student[students.length];
-        try {
-            if(students.length < 15 && !isInGroup(student)) {
-                studentsNew = new Student[students.length + 1];
-                for(int i = 0; i < students.length; i++) {
-                    studentsNew[i] = students[i];
-                }
-                studentsNew[students.length-1] = student;
-                students = studentsNew;
-            }
-        } catch (IllegalStateException e) {
-            if (students.length >= 15) {
-                System.out.println("To many students in that group. Student can't be added");
-            }
-            if(isInGroup(student)) {
-                System.out.println("Student already added");
-            }
-            e.printStackTrace();
-        }
-        return studentsNew;
-    }
 }
